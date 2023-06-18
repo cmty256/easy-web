f52c535dab729b2c7b61d2453ff8ea9506b9282a
chenmeng <2691357857@qq.com>
2023-06-18 15:38:08 +0800

    初始化

--- src/main/java/com/chenmeng/project/model/entity/User.java
+++ src/main/java/com/chenmeng/project/model/entity/User.java
@@ -1,9 +1,7 @@
 package com.chenmeng.project.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
@@ -78,7 +76,10 @@ public class User implements Serializable {
 
     /**
      * 是否删除：0 - 未删；1 - 已删
+     *
+     * 注解：逻辑删除
      */
+    @TableLogic
     private Integer isDelete;
 
     @TableField(exist = false)
