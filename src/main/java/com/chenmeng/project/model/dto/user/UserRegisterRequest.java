1b0c79aba2990fe7f3be60ce559cb85eeaed25e7
chenmeng <2691357857@qq.com>
2023-06-18 15:33:56 +0800

    初始化

--- src/main/java/com/chenmeng/project/model/dto/user/UserRegisterRequest.java
+++ src/main/java/com/chenmeng/project/model/dto/user/UserRegisterRequest.java
@@ -4,10 +4,16 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * 用户注册请求体
+ *
+ * @author 沉梦听雨
+ * @date 2023/06/18
+ */
 @Data
 public class UserRegisterRequest implements Serializable {
 
-    private static final long serialVersionUID = 3191241716373120793L;
+    private static final long serialVersionUID = 3191241716373120793L; // 序列化版本号
 
     private String userAccount;
 
